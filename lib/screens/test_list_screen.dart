ac23451bd7d6b5c44e139ea51285d392bd9c0807
studybuild576-commits <studybuild576@gmail.com>
2025-09-02 11:40:56 +0530

    Create test_list_screen.dart

---
+++ lib/screens/test_list_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../models/exam.dart';
+import 'test_screen.dart';
+
+class TestListScreen extends StatelessWidget {
+  final Exam exam;
+  const TestListScreen({super.key, required this.exam});
+
+  final List<String> testTypes = const [
+    'Model Test',
+    'PYQ Test',
+    'Sectional Test',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${exam.name} - Tests'),
+      ),
+      body: ListView.builder(
+        itemCount: testTypes.length,
+        itemBuilder: (context, index) {
+          final testType = testTypes[index];
+          return ListTile(
+            title: Text(testType),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => TestScreen(exam: exam, testType: testType),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
