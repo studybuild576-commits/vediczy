1cca63545e08bdf888c02634bb9fa5621fac33b0
studybuild576-commits <studybuild576@gmail.com>
2025-09-03 08:26:43 +0530

    Update test_list_screen.dart

--- lib/screens/test_list_screen.dart
+++ lib/screens/test_list_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vediczy/screens/cbt_realistic_screen.dart';
+import 'package:vediczy/screens/realistic_cbt_test_screen.dart';
 import '../models/exam.dart';
 import 'realistic_cbt_test_screen.dart'; // Nayi screen ka import
 
