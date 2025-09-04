1467093dbdd79d4b64f8eb24de143b191777c59f
studybuild576-commits <studybuild576@gmail.com>
2025-09-04 10:57:06 +0530

    Create test_model.dart

---
+++ lib/models/test_model.dart
@@ -0,0 +1,17 @@
+class Test {
+  final String id;
+  final String title;
+  final String description;
+  final int durationInMinutes;
+  final int totalMarks;
+  final String examName;
+
+  Test({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.durationInMinutes,
+    required this.totalMarks,
+    required this.examName,
+  });
+}
