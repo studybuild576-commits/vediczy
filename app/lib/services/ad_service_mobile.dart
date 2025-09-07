89fc44357e9569cdd597dfc300ab3cba3e6ca333
studybuild576-commits <studybuild576@gmail.com>
2025-09-07 18:42:37 +0530

    Update ad_service_mobile.dart

--- app/lib/services/ad_service_mobile.dart
+++ app/lib/services/ad_service_mobile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package.google_mobile_ads/google_mobile_ads.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart'; // YEH IMPORT SAHI HONA CHAHIYE
 
 class AdService {
   // Google ki Test Ad Unit IDs
