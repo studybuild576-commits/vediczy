b2e791ab415aeebc835386b655775a86a6637fe8
studybuild576-commits <studybuild576@gmail.com>
2025-09-07 19:52:56 +0530

    Update ad_service_stub.dart

--- app/lib/services/ad_service_stub.dart
+++ app/lib/services/ad_service_stub.dart
@@ -1,4 +1,16 @@
 // File: app/lib/services/ad_service_stub.dart
+// ignore_for_file: unused_import
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+// यह क्लास खाली रहेगी और कभी इस्तेमाल नहीं होगी।
+// यह सिर्फ़ conditional import को सही से काम करने में मदद करती है।
 class AdService {
-  // Yeh class khaali rahegi
+  BannerAd? get bannerAd => null;
+  static Future<void> initialize() async {}
+  void loadBannerAd() {}
+  void loadInterstitialAd() {}
+  void showInterstitialAd({required VoidCallback onAdDismissed}) { onAdDismissed(); }
+  void loadRewardedAd() {}
+  void showRewardedAd({required Function(RewardItem) onUserEarnedReward}) {}
 }
