5dad0f555a142165a381e4efd745f8eea9d9eead
studybuild576-commits <studybuild576@gmail.com>
2025-09-07 20:36:13 +0530

    Update ad_service_web.dart

--- app/lib/services/ad_service_web.dart
+++ app/lib/services/ad_service_web.dart
@@ -1,33 +1,30 @@
-// File: app/lib/services/ad_service_web.dart
+// lib/services/ad_service_web.dart
 import 'package:flutter/material.dart';
 
-// Dummy class with unique name
-class WebRewardItem {
+// Dummy RewardItem same नाम से बनाओ
+class RewardItem {
   final int amount;
   final String type;
 
-  WebRewardItem({this.amount = 1, this.type = "web_reward"});
+  RewardItem({this.amount = 0, this.type = "Web"});
 }
 
 class AdService {
   dynamic get bannerAd => null;
 
-  static Future<void> initialize() async {
-    // Web par kuch nahi karna
-  }
+  static Future<void> initialize() async {}
 
   void loadBannerAd() {}
-
   void loadInterstitialAd() {}
 
   void showInterstitialAd({required VoidCallback onAdDismissed}) {
     onAdDismissed();
   }
-  
+
   void loadRewardedAd() {}
 
-  void showRewardedAd({required Function(WebRewardItem) onUserEarnedReward}) {
-    // Web par fake reward bhej dete hain
-    onUserEarnedReward(WebRewardItem());
+  void showRewardedAd({required Function(RewardItem) onUserEarnedReward}) {
+    // Web par kuch nahi, but dummy reward bhej do
+    onUserEarnedReward(RewardItem(amount: 0, type: "web"));
   }
 }
