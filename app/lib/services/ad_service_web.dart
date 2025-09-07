d1bbb660fa69b2d5ee1d20184a6c6d8727f36fd6
studybuild576-commits <studybuild576@gmail.com>
2025-09-07 21:32:25 +0530

    Update ad_service_web.dart

--- app/lib/services/ad_service_web.dart
+++ app/lib/services/ad_service_web.dart
@@ -1,30 +1,40 @@
-// lib/services/ad_service_web.dart
 import 'package:flutter/material.dart';
 
-// Dummy RewardItem same नाम से बनाओ
-class RewardItem {
+// Dummy class, taaki code mein error na aaye
+class WebRewardItem {
   final int amount;
   final String type;
 
-  RewardItem({this.amount = 0, this.type = "Web"});
+  WebRewardItem({this.amount = 0, this.type = "Web"});
 }
 
 class AdService {
+  // Web par koi banner nahi hai, isliye null bhej rahe hain
   dynamic get bannerAd => null;
 
-  static Future<void> initialize() async {}
+  static Future<void> initialize() async {
+    // Web par kuch nahi karna
+  }
 
-  void loadBannerAd() {}
-  void loadInterstitialAd() {}
+  void loadBannerAd() {
+    // Web par kuch nahi karna
+  }
+
+  void loadInterstitialAd() {
+    // Web par kuch nahi karna
+  }
 
   void showInterstitialAd({required VoidCallback onAdDismissed}) {
+    // Ad na hone par seedhe aage badh jaayein
     onAdDismissed();
   }
 
-  void loadRewardedAd() {}
+  void loadRewardedAd() {
+    // Web par kuch nahi karna
+  }
 
-  void showRewardedAd({required Function(RewardItem) onUserEarnedReward}) {
-    // Web par kuch nahi, but dummy reward bhej do
-    onUserEarnedReward(RewardItem(amount: 0, type: "web"));
+  void showRewardedAd({required Function(WebRewardItem) onUserEarnedReward}) {
+    // Web par ad nahi, seedhe dummy reward bhej do
+    onUserEarnedReward(WebRewardItem());
   }
 }
