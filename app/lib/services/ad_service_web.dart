feb057c7571c6f925ada7d8f52e7adba24068d5b
studybuild576-commits <studybuild576@gmail.com>
2025-09-07 20:22:07 +0530

    Update ad_service_web.dart

--- app/lib/services/ad_service_web.dart
+++ app/lib/services/ad_service_web.dart
@@ -1,36 +1,33 @@
 // File: app/lib/services/ad_service_web.dart
 import 'package:flutter/material.dart';
-// Note: Hum yahan google_mobile_ads ko import nahi kar rahe hain
 
-// Dummy class, taaki code mein error na aaye
-class RewardItem {}
+// Dummy class with unique name
+class WebRewardItem {
+  final int amount;
+  final String type;
+
+  WebRewardItem({this.amount = 1, this.type = "web_reward"});
+}
 
 class AdService {
-  // Web par koi banner nahi hai, isliye null bhej rahe hain
   dynamic get bannerAd => null;
 
   static Future<void> initialize() async {
     // Web par kuch nahi karna
   }
 
-  void loadBannerAd() {
-    // Web par kuch nahi karna
-  }
+  void loadBannerAd() {}
 
-  void loadInterstitialAd() {
-    // Web par kuch nahi karna
-  }
+  void loadInterstitialAd() {}
 
   void showInterstitialAd({required VoidCallback onAdDismissed}) {
-    // Ad na hone par seedhe aage badh jaayein
     onAdDismissed();
   }
   
-  void loadRewardedAd() {
-    // Web par kuch nahi karna
-  }
+  void loadRewardedAd() {}
 
-  void showRewardedAd({required Function(RewardItem) onUserEarnedReward}) {
-    // Web par kuch nahi karna
+  void showRewardedAd({required Function(WebRewardItem) onUserEarnedReward}) {
+    // Web par fake reward bhej dete hain
+    onUserEarnedReward(WebRewardItem());
   }
 }
