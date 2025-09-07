d1da3a528e4253fad937782382131e680c19a4b3
studybuild576-commits <studybuild576@gmail.com>
2025-09-07 09:44:11 +0530

    Create main_layout.dart

---
+++ admin/lib/screens/main_layout.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:vediczy_admin/screens/dashboard_screen.dart';
+import 'package:vediczy_admin/screens/manage_tests_screen.dart';
+
+class MainLayout extends StatefulWidget {
+  @override
+  _MainLayoutState createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  Widget _currentPage = DashboardScreen();
+
+  void _navigateTo(Widget page) {
+    if (Navigator.of(context).canPop()) {
+      Navigator.of(context).pop();
+    }
+    setState(() {
+      _currentPage = page;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vediczy Admin Panel"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.indigo),
+              child: Text('Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
+            ),
+            ListTile(
+              leading: Icon(Icons.dashboard),
+              title: const Text('Dashboard'),
+              onTap: () => _navigateTo(DashboardScreen()),
+            ),
+            ListTile(
+              leading: Icon(Icons.article),
+              title: const Text('Manage Tests'),
+              onTap: () => _navigateTo(ManageTestsScreen()),
+            ),
+          ],
+        ),
+      ),
+      body: _currentPage,
+    );
+  }
+}
